f9cc8f14f2f5ff7d0dc0883e4ea54cf96edc5480
João paulo <joaopauloamesco@gmail.com>
2018-02-05 14:26:07 -0200

    HelloWorld, Post e Get

---
+++ Treino CF/PostGetModel/PostGetModel/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PostGetModel.Models;
+
+namespace PostGetModel.Controllers
+{
+    public class HomeController : Controller
+    {
+   
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa
+            {
+                PessoaID = 1,
+                Nome = "Cleyton Ferrari",
+                Twitter = "@cleytonferrari"
+            };
+            
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            
+            return View(pessoa);
+        }
+    }
+}
